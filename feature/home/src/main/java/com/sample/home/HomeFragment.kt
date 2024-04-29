2199c9881a9c7b957b5ec1a8b3aef09648e30951
Giovanni Junseo Kim <kingjunseo@gmail.com>
2024-04-29 20:38:33 +0900

    Change Activity to Fragment (#4)
    * Change Activity to Fragment
    
    * Change view's ids

---
+++ feature/home/src/main/java/com/sample/home/HomeFragment.kt
@@ -0,0 +1,36 @@
+package com.sample.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.sample.home.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+    private var _binding: FragmentHomeBinding? = null
+    private val binding = requireNotNull(_binding)
+    private val viewModel: HomeViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.vm = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+}
--- feature/home/src/main/java/com/sample/home/HomeViewModel.kt
+++ feature/home/src/main/java/com/sample/home/HomeViewModel.kt
@@ -3,7 +3,7 @@ package com.sample.home
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class MainViewModel : ViewModel() {
+class HomeViewModel : ViewModel() {
     val receivedNumber: MutableLiveData<Int> = MutableLiveData()
     val numberToGive: MutableLiveData<String> = MutableLiveData("")
 
